be95da52a4a1f9dfa6745d1d74ee5a797208e8d1
afroll <aafroll@gmail.com>
2020-03-24 16:30:48 +0300

    commit

--- PyMar2/240320/test.py
+++ PyMar2/240320/test.py
@@ -4,10 +4,24 @@ cur = conn.cursor()
 query = "CREATE TABLE IF NOT EXISTS films (id INT, title TEXT, duration INT)"
 cur.execute(query)
 
-insert_query = "INSERT INTO films VALUES(1, 'LOTR', 220)"
-cur.execute(insert_query)
+insert_query = "INSERT INTO films VALUES(?,?,?)"
+cur.execute(insert_query, (2, 'HP', 178))
 conn.commit()
 
 select_query = "SELECT * FROM films"
 for row in  cur.execute(select_query):
     print(row)
+
+update_query = "UPDATE films SET duration=? WHERE title=?"
+cur.execute(update_query,(920, 'HP'))
+conn.commit()
+
+delete_query = "DELETE FROM films"
+cur.execute(delete_query)
+conn.commit()
+
+select_query = "SELECT * FROM films"
+for row in cur.execute(select_query):
+    print(row)
+
+conn.close()
