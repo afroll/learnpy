9484b24023f0ad3f6963a3d2496d322bc5167636
Andrei <3332221+afroll@users.noreply.github.com>
2021-04-14 13:37:10 +0300

    delete folder PyMar2

--- PyMar2/230320/task13.py
+++
@@ -1,4 +0,0 @@
-def a_def(x,y):
-    return x + y
-a_lambda = lambda x,y: x + y
-print(a_lambda(1,2))
--- PyMar2/240320/test_equation.py
+++
@@ -1,8 +0,0 @@
-import equation as e
-
-def test_add():
-    assert e.add(2,3) == 5
-    assert e.add(3,3) == 6
-    assert e.add(10,-10) == 0
-    for i in range(0, 100):
-        assert e.add(i, i) == 2*i
